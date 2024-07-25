6f34dc625e59f78c4fcef76450b66478c59a7790
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2024-07-25 12:41:33 +0300

    Initial Json Implementation

--- category.js
+++ category.js
@@ -15,3 +15,56 @@ function search(){
     }
  
 }
+
+// Json Loader for Products
+let products = null;
+fetch('items.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+    })
+
+function addDataToHTML() {
+    let listProductHTML = document.querySelector('.cardsLoader');
+
+    {
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.href = './details.html?id=' + product.id;
+            newProduct.classList.add('card');
+            newProduct.innerHTML =
+            `<div class="img">
+           
+            <img src="${product.image}" alt="">
+             </div>
+        <div class="text">
+            <p>${product.category}</p>
+            <h4>${product.name}</h4>
+            <div class="rating">
+                <p>${product.rating}</p>
+                <div class="icon">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+            </div>
+          
+        </div>`
+            listProductHTML.appendChild(newProduct);
+
+        });
+    }
+}
+//Storege
+function addItems(){
+    const names=document.getElementById('productName').value;
+    const rating=document.getElementById('productRating').value;
+    const category=document.getElementById('productCategory').value;
+    console.log(names);
+    console.log(rating);
+    console.log(category);
+    
+}
